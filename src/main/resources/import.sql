fee98383d9c3c7d7ca351c2cfe2c28173d014e71
Olafur Georg <ogg4@hi.is>
2017-10-03 12:29:36 +0000

    changed dummy users and passwords

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,8 +3,8 @@ insert into role (name) values ('ROLE_USER');
 insert into role (name) values ('ROLE_ADMIN');
 
 -- Insert two users (passwords are both 'password')
-insert into nord_user (username,enabled,password,role_id) values ('u',true,'$2a$10$jKsL6a/wqwzTYiZGUFecQOSIHpYwkpJm8n2VRgYNsQ/XD7GP1CLv2',1);
-insert into nord_user (username,enabled,password,role_id) values ('u2',true,'$2a$10$jKsL6a/wqwzTYiZGUFecQOSIHpYwkpJm8n2VRgYNsQ/XD7GP1CLv2',1);
-insert into nord_user (username,enabled,password,role_id) values ('a',true,'$2a$10$jKsL6a/wqwzTYiZGUFecQOSIHpYwkpJm8n2VRgYNsQ/XD7GP1CLv2',2);
-insert into nord_user (username,enabled,password,role_id) values ('a2',true,'$2a$10$jKsL6a/wqwzTYiZGUFecQOSIHpYwkpJm8n2VRgYNsQ/XD7GP1CLv2',2);
+insert into nord_user (username,enabled,password,role_id) values ('user1',true,'$2a$06$XXGRAjNiKfgXsUhDwghRIe0QstIDnXoFw6JvnpM/pTRFKQsUmrUcW',1);
+insert into nord_user (username,enabled,password,role_id) values ('user2',true,'$2a$06$XXGRAjNiKfgXsUhDwghRIe0QstIDnXoFw6JvnpM/pTRFKQsUmrUcW',1);
+insert into nord_user (username,enabled,password,role_id) values ('admin1',true,'$2a$06$XXGRAjNiKfgXsUhDwghRIe0QstIDnXoFw6JvnpM/pTRFKQsUmrUcW',2);
+insert into nord_user (username,enabled,password,role_id) values ('admin2',true,'$2a$06$XXGRAjNiKfgXsUhDwghRIe0QstIDnXoFw6JvnpM/pTRFKQsUmrUcW',2);
 
