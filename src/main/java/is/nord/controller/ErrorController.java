698523d0900df04a0c899634890baf550d50faac
stellarutg <stellarut95@gmail.com>
2017-11-08 19:09:46 +0000

    Fixed security, not everyone can access admin or user pages, special error page for white-label errors

---
+++ src/main/java/is/nord/controller/ErrorController.java
@@ -0,0 +1,56 @@
+package is.nord.controller;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController {
+
+    private static final String PATH = "/error";
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    /**
+     * Meðhöndlar villu sem verður á þjóninum
+     * @param request  beiðnin með villuskilaboðum
+     * @return
+     */
+    @RequestMapping(value = PATH)
+    public ModelAndView villa(HttpServletRequest request) {
+
+        // sendir attribute til viðmótsins og birtir síðuna með villuskilaboðum
+        return new ModelAndView("error", "attrs", getErrorAttributes(request, false));
+    }
+
+    /**
+     * Skilar slóðinni fyrir villu
+     */
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+
+    /**
+     * Nær í villu-attribute sem komu út úr Http beiðni
+     * @param request  Http beiðnin
+     * @param includeStackTrace ef true þá er skilað stack trace annars ekki
+     * @return mengi af tvíundum með nafni af attributi og gildi þeirra
+     */
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request,
+                                                   boolean includeStackTrace) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        return this.errorAttributes.getErrorAttributes(requestAttributes,
+                includeStackTrace);
+    }
+}
